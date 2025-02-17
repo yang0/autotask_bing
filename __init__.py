5745f3dbeefd8467c6365a7df503561d15f816e8
yang0 <yang0@126.com>
2025-02-17 13:29:13 +0800

    sdf

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .nodes.news import *
 from .envParams import *
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 GIT_URL = "https://github.com/yang0/autotask_bing"
 NAME = "Bing News Search"
 DESCRIPTION = "search news from bing"
-TAGS = ["news", "search", "bing"]
+TAGS = ["News", "search", "bing"]
 
