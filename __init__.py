b776e5387e7ae01d636f35aab4a733c8e2b5e5a5
yang0 <yang0@126.com>
2025-02-17 13:49:01 +0800

    sd

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .nodes.news import *
 from .envParams import *
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 GIT_URL = "https://github.com/yang0/autotask_bing"
 NAME = "Bing News Search"
 DESCRIPTION = "search news from bing"
-TAGS = ["News", "search", "bing"]
+TAGS = ["news", "search", "bing"]
 
